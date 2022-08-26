53397554d50c986deab10b1bf0e8f3b47fc4333f
Guy Royse <guy@guyroyse.com>
2022-08-26 15:06:50 -0400

    look and whisper and refactor

--- spec/session.spec.js
+++ spec/session.spec.js
@@ -3,27 +3,34 @@ import { redisClient } from '$lib/redis.js'
 
 describe("Session", () => {
 
-  let session, otherSession
+  let mephalichSession, conradSession, leeroySession
 
   beforeAll(async () => {
     await Promise.all([
       redisClient.json.set('user:mephalich', '$', { name: 'Mephalich'}),
-      redisClient.json.set('user:conrad', '$', { name: 'Conrad Taylor'}),
-      redisClient.json.set('room:00000000000000000000000001', '$', { name: 'The Hub'})
+      redisClient.json.set('user:conrad', '$', { name: 'Conrad'}),
+      redisClient.json.set('user:leeroy', '$', { name: 'Leeroy'}),
+      redisClient.json.set('room:00000000000000000000000001', '$', {
+        name: 'The Hub',
+        description: 'This large, open, circular room has numerous doors. At the center, there is a large, oak reception desk.' })
     ])
   })
 
   beforeEach(async () => {
-    session = new Session()
-    await session.init('mephalich')
+    mephalichSession = new Session()
+    await mephalichSession.init('mephalich')
 
-    otherSession = new Session()
-    await otherSession.init('conrad')
+    conradSession = new Session()
+    await conradSession.init('conrad')
+
+    leeroySession = new Session()
+    await leeroySession.init('leeroy')
   })
 
   afterEach(() => {
-    session.destroy()
-    otherSession.destroy()
+    mephalichSession.destroy()
+    conradSession.destroy()
+    leeroySession.destroy()
   })
 
   afterAll(async () => {
@@ -32,23 +39,58 @@ describe("Session", () => {
 
   it("recevies a chat message from ourselves", async () => {
     return new Promise((resolve, reject) => {
-      session.onMessage((message, channel) => {
-        expect(channel).toBe(session.room.id)
+      mephalichSession.onMessage((message, channel) => {
+        expect(channel).toBe(mephalichSession.room.id)
         expect(message).toBe('Mephalich: foo')
         resolve()
       })
-      session.sendCommand('foo')
+      mephalichSession.sendCommand('foo')
     })
   })
 
   it("recevies a chat message from someone else", async () => {
     return new Promise((resolve, reject) => {
-      session.onMessage((message, channel) => {
-        expect(channel).toBe(session.room.id)
-        expect(message).toBe('Conrad Taylor: bar')
+      conradSession.onMessage((message, channel) => {
+        expect(channel).toBe(conradSession.room.id)
+        expect(message).toBe('Mephalich: bar')
         resolve()
       })
-      otherSession.sendCommand('bar')
+      mephalichSession.sendCommand('bar')
+    })
+  })
+
+  it("recevies a whisper message from ourselves", async () => {
+    return new Promise((resolve, reject) => {
+      mephalichSession.onMessage((message, channel) => {
+        expect(channel).toBe(mephalichSession.user.id)
+        expect(message).toBe('Mephalich (to Conrad): baz qux')
+        resolve()
+      })
+      mephalichSession.sendCommand('/whisper conrad baz qux')
+    })
+  })
+
+  it("recevies a whisper message directed at us and only us", async () => {
+    return new Promise((resolve, reject) => {
+      conradSession.onMessage((message, channel) => {
+        expect(channel).toBe(conradSession.user.id)
+        expect(message).toBe('Mephalich (to Conrad): baz qux')
+        resolve()
+      })
+      mephalichSession.sendCommand('/whisper leeroy baz qux')
+      mephalichSession.sendCommand('/whisper conrad baz qux')
+    })
+  })
+
+  it("recevies a room description directed at us and only us when the user looks", async () => {
+    return new Promise((resolve, reject) => {
+      mephalichSession.onMessage((message, channel) => {
+        expect(channel).toBe(mephalichSession.user.id)
+        expect(message).toBe('The Hub\nThis large, open, circular room has numerous doors. At the center, there is a large, oak reception desk.')
+        resolve()
+      })
+      leeroySession.sendCommand('/look')
+      mephalichSession.sendCommand('/look')
     })
   })
 })
