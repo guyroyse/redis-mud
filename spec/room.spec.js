b19b8de22ee1d30974243411e6069236aeb7ec06
Guy Royse <guy@guyroyse.com>
2022-09-02 14:43:43 -0400

    tested fetch, added massive destruction

--- spec/room.spec.js
+++ spec/room.spec.js
@@ -2,36 +2,74 @@ import { Room } from '$lib/room.js'
 import { redisClient } from '$lib/redis.js'
 import { afterEach, describe } from 'vitest'
 
+const ROOM_NAME = "The Pit"
+const ROOM_DESCRIPTION = "A deep, dark pit stares back at you."
+
 describe("Room", () => {
 
-  let room
+  let createdRoom, fetchedRoom
 
   describe("when created", () => {
 
-    beforeEach(async () => {
-      room = await Room.create()
-    })
+    beforeEach(async () => { createdRoom = await Room.create() })
+    afterEach(async () => { await redisClient.unlink(`Room:${createdRoom.id}`) })
 
-    afterEach(async () => {
-      await redisClient.unlink(`Room:${room.id}`)
-    })
-
-    it("has a valid id", () => expect(room.id).toMatch(/^[0-9A-HJKMNP-TV-Z]{26}$/))
-    it("has an empty name", async () => expect(room.name()).resolves.toBeNull())
-    it("has an empty description", async () => expect(await room.description()).toBeNull())
+    it("has a valid id", () => expect(createdRoom.id).toMatch(/^[0-9A-HJKMNP-TV-Z]{26}$/))
+    it("has an empty name", async () => expect(createdRoom.name()).resolves.toBeNull())
+    it("has an empty description", async () => expect(await createdRoom.description()).toBeNull())
 
     it("creates an empty document in Redis", async () => {
-      const roomJson = await redisClient.json.get(`Room:${room.id}`, '$')
-      expect(roomJson).toEqual({ id: room.id })
+      const roomJson = await redisClient.json.get(`Room:${createdRoom.id}`, '$')
+      expect(roomJson).toEqual({ id: createdRoom.id })
     })
 
     describe("when the properties are updated", () => {
+
       beforeEach(async () => {
-        await room.name("The Pit")
-        await room.description("A deep, dark pit stares back at you.")
+        await createdRoom.name(ROOM_NAME)
+        await createdRoom.description(ROOM_DESCRIPTION)
       })
-      it("has the expected name", async () => expect(room.name()).resolves.toBe("The Pit"))
-      it("has the expected description", async () => expect(room.description()).resolves.toBe("A deep, dark pit stares back at you."))
+
+      it("has the expected name", async () => expect(createdRoom.name()).resolves.toBe(ROOM_NAME))
+      it("has the expected description", async () => expect(createdRoom.description()).resolves.toBe(ROOM_DESCRIPTION))
+
+      it("has the expected document in Redis", async () => {
+        const roomJson = await redisClient.json.get(`Room:${createdRoom.id}`, '$')
+        expect(roomJson).toEqual({
+          id: createdRoom.id,
+          name: ROOM_NAME,
+          description: ROOM_DESCRIPTION
+        })
+      })
+
+      describe("when fetched", () => {
+
+        beforeEach(() => { fetchedRoom = Room.fetch(createdRoom.id) })
+
+        it("has the expected id", () => expect(fetchedRoom.id).toBe(createdRoom.id))
+        it("has the expected name", async () => expect(fetchedRoom.name()).resolves.toBe(ROOM_NAME))
+        it("has the expected description", async () => expect(fetchedRoom.description()).resolves.toBe(ROOM_DESCRIPTION))
+
+        describe("when destroyed", () => {
+
+          beforeEach(async () => { await fetchedRoom.destroy() })
+
+          it("has nothing in Redis", async () => {
+            const exists = await redisClient.exists(`Room:${fetchedRoom.id}`)
+            expect(exists).toBe(0)
+          })
+        })
+      })
+
+      describe("when destroyed", () => {
+
+        beforeEach(async () => { await Room.destroy(createdRoom.id) })
+
+        it("has nothing in Redis", async () => {
+          const exists = await redisClient.exists(`Room:${createdRoom.id}`)
+          expect(exists).toBe(0)
+        })
+      })
     })
   })
 })
